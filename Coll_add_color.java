15a404b04987d787225d89073b7fecbcd9b07646
SURYA K <68805916+suryakathire@users.noreply.github.com>
2021-05-26 00:00:48 +0530

    Add files via upload

---
+++ Coll_add_color.java
@@ -0,0 +1,18 @@
+ 
+package coll_add_color;
+ import java.util.*;
+public class Coll_add_color {
+
+    
+    public static void main(String[] args) {
+         ArrayList <String> color=new ArrayList<String>();
+         color.add("yellow");
+         color.add("red");
+         color.add("green");
+         color.add("blue");
+         System.out.println(color);
+         
+         
+    }
+    
+}
